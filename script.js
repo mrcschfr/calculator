a6d1ffd49b45e057ea4258cd8879ded351be0f8f
Marc Schaefer <hi@marcschaefer.de>
2021-05-09 13:51:44 +0200

    Implement first iteration of mobile layout

---
+++ script.js
@@ -0,0 +1,42 @@
+// Helper functions
+
+function add(num1, num2) {
+    return Number(num1) + Number(num2);
+}
+
+function subtract(num1, num2) {
+    return Number(num1) - Number(num2);
+}
+
+function multiply(num1, num2) {
+    return Number(num1) * Number(num2);
+}
+
+function divide(num1, num2) {
+    return Number(num1) / Number(num2);
+}
+
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case 'add':
+            return add(num1, num2);
+            break;
+        case 'subtract':
+            return subtract(num1, num2);
+            break;
+        case 'multiply':
+            return multiply(num1, num2);
+            break;
+        case 'divide':
+            return divide(num1, num2);
+            break;
+        default:
+            break;
+    }
+}
+
+document.getElementById('onoff').addEventListener('click', (e) => {
+    console.log("yeee");
+    document.getElementById('toggle').classList.toggle('on');
+    document.getElementById('display').classList.toggle('on');
+});
